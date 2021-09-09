96f6ce4bd8a39efece2308389531e37c624ff058
David Moina <davemolina777@gmail.com>
2021-09-09 09:19:37 -0500

    commit tree 423a0691174e794b87ba6b76c36ffd2863ca61f7

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+class EyesOnMe extends Component {1
+    handleOnBlur = () => {
+        console.log("Hey! Eyes on me!")
+    }
+    handleOnFocus = () => {
+        console.log("Good!")
+    }
+    render() {
+        return (
+            <div>
+                <button onFocus={this.handleOnFocus} onBlur={this.handleOnBlur}>Button</button>
+            </div>
+        )
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component {
+    handleKeyUp = () => {
+        console.log("Entering password...")
+    }
+    render() {
+        return(
+            <div>
+                <input type="password" name="keypad" value="" onKeyUp={this.handleKeyUp}/>
+            </div>
+        )
+    }
+}
+
+export default Keypad
